c96bc11da52629bffa0d4e98746ec0974af580b8
RenataARocha <programadorawebrenatarocha@gmail.com>
2025-12-26 23:05:18 -0300

    refactor: melhora legibilidade do JS com uso de variáveis

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-document.getElementById("botaoEnviar").addEventListener(
+let nomeInput = document.getElementById("nome-usuario")
+let emailInput = document.getElementById("email-usuario")
+let telefoneInput = document.getElementById("telefone-usuario")
+let botaoEnviar = document.getElementById("botaoEnviar")
+
+
+
+botaoEnviar.addEventListener(
     "click", validaFormulario
 )
 
 function validaFormulario() {
-    if (document.getElementById("nome-usuario").value != "" &&
-        document.getElementById("email-usuario").value != "" &&
-        document.getElementById("telefone-usuario").value != "") {
+    if (
+        nomeInput.value != "" &&
+        emailInput.value != "" &&
+        telefoneInput.value != "") {
         alert("Prontinho! Você receberá as novidades por email.");
     } else {
         alert("Por favor, preencha os campos nome e email!");
